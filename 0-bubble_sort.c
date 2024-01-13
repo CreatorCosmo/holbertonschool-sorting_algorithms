58a8baf6fbd4801d9cea8b4cd6ee765b89df085e
CreatorCosmo <girizarrymartell14@gmail.com>
2024-01-12 20:33:06 -0400

    Cleaned code to abide with Betty coding style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,31 +7,35 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void bubble_sort(int *array, size_t size) {
-    size_t i, j;
-    int swapped, temp;
+void bubble_sort(int *array, size_t size)
+	{
+	size_t i, j;
+	int swapped, temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+	return;
 
-    /* Variable declarations at the beginning of the block */
-    for (i = 0; i < size - 1; i++) {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-                print_array(array, size); 
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+	swapped = 0;
+	for (j = 0; j < size - i - 1; j++)
+	{
+
+	if (array[j] > array[j + 1])
+	{
+		temp = array[j];
+		array[j] = array[j + 1];
+		array[j + 1] = temp;
+		swapped = 1;
+		print_array(array, size);
+		}
+	}
+	if (!swapped)
+	break;
+	}
 }
