33d9889ed86d05353cb458e384cbbbc475a3c14c
CreatorCosmo <girizarrymartell14@gmail.com>
2024-01-12 20:11:00 -0400

    Updated function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * swap_ints - Swaps two integers in an array
  *
@@ -20,24 +19,25 @@ void swap_ints(int *a, int *b)
  * @array: Array of integers
  * @size: Size of the array
  */
-void bubble_sort(int *array, size_t size)
-{
-    size_t i, j;
-    int swapped;
 
-    for (i = 0; i < size - 1; i++)
-    {
+void bubble_sort(int *array, size_t size) {
+    if (array == NULL || size < 2)
+        return;
+
+    int swapped;
+    for (size_t i = 0; i < size - 1; i++) {
         swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap_ints(&array[j], &array[j + 1]);
+        for (size_t j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
                 swapped = 1;
+                print_array(array, size); // Print the array after each swap
             }
         }
-        print_array(array, size);
-        if (swapped == 0)
+        if (swapped == 0) { // If no two elements were swapped by inner loop, then break
             break;
+        }
     }
 }
